733463b2b452094b6928779b10bd5e8fb89d42ad
Xueyong Zhi <yong@mac.local>
2011-04-26 14:42:36 -0400

    fix loading issue

--- lib/publish2cloud/tasks.rb
+++ lib/publish2cloud/tasks.rb
@@ -1,8 +1,8 @@
-require 'publish2cloud'
+require File.expand_path(File.join(File.dirname(__FILE__), '..', 'publish2cloud', 'publish2cloud'))
 
 namespace :p2c do
   desc "Publish2Cloud"
   task :push do
-    puts "ok"
+    p2c = Publish2Cloud::Publisher.new("", "", "", "")
   end
 end
